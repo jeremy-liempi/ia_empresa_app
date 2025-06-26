a2f1cd3a13f48ee677d552ee1de7984aa81ee419
jeremy-liempi <jeremy3liempi@gmail.com>
2025-06-25 21:32:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -79,13 +79,12 @@ elif seccion == "Gestión de empleados":
             años = st.number_input("Años de experiencia", min_value=0, max_value=50)
             horas = st.number_input("Horas disponibles por semana", min_value=0, max_value=168)
             skills = st.text_input("Skills (separadas por comas)")
-         
-        estado_seleccionado = st.selectbox("Estado", ["Disponible", "En proyecto", "No disponible"], key="estado_outside")
+            estado_seleccionado = st.selectbox("Estado", ["Disponible", "En proyecto", "No disponible"], key="estado_outside")
     
-        # Si está en proyecto, pedir estos datos también fuera
-        proyecto_actual = None
-        inicio_proyecto = None
-        fin_proyecto = None
+            # Si está en proyecto, pedir estos datos también fuera
+            proyecto_actual = None
+            inicio_proyecto = None
+            fin_proyecto = None
             
             cv = st.file_uploader("Cargar CV (PDF)", type=["pdf"])
     
